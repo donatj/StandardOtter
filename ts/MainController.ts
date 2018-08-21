5485962d40b527434f8ae6a454d55e48859887fe
Jesse Donat <jdonat@myon.com>
2018-08-21 13:18:08 -0500

    Initial Electron rewrite

---
+++ ts/MainController.ts
@@ -0,0 +1,56 @@
+import child_process = require('child_process');
+
+export class MainController {
+
+	private timeout = 0;
+
+	constructor(public editor: monaco.editor.IStandaloneCodeEditor, private cmd: HTMLInputElement, private stdOut: HTMLTextAreaElement, private stdErr: HTMLTextAreaElement) {
+
+		editor.onDidChangeModelContent((e) => {
+			clearTimeout(this.timeout);
+			this.timeout = window.setTimeout(this.doExec.bind(this), 300);
+		});
+
+		window.addEventListener('resize', () => {
+			editor.layout();
+		});
+	}
+
+	private doExec(){
+		let result = this.execute(this.cmd.value, this.editor.getValue());
+
+		this.stdOut.textContent = (result.stdout || "").toString();
+		this.stdErr.textContent = (result.stderr || "").toString();
+
+		if(result.status > 0) {
+			this.stdOut.style.background = '#fee';
+			this.stdErr.style.background = '#fee';
+
+			document.title = 'Exit Code: ' + result.status;
+		}else{
+			this.stdOut.style.background = '';
+			this.stdErr.style.background = '';
+
+			document.title = 'Standard Otter';
+		}
+
+		if( result.status !== null ) {
+			this.cmd.style.background = '';
+		} else {
+			this.cmd.style.background = '#fee';
+		}
+	}
+
+	private execute(cmd: string, input: string) {
+		let cmdData = cmd.split(' ').filter(function (x) { return x != '' });
+
+		let result = child_process.spawnSync(
+			cmdData.shift() || "cat",
+			cmdData,
+			{ input }
+		);
+
+		return result;
+	}
+
+}
