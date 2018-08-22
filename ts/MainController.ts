99dd0fcc3f3ba3d57597ccfb9c998fadba8ae265
Jesse Donat <jdonat@myon.com>
2018-08-22 11:25:34 -0500

    Hide stderr when no stderr

--- ts/MainController.ts
+++ ts/MainController.ts
@@ -4,7 +4,13 @@ export class MainController {
 
 	private timeout = 0;
 
-	constructor(public editor: monaco.editor.IStandaloneCodeEditor, private cmd: HTMLInputElement, private stdOut: HTMLTextAreaElement, private stdErr: HTMLTextAreaElement) {
+	constructor(
+		public editor: monaco.editor.IStandaloneCodeEditor,
+		private container: HTMLElement,
+		private cmd: HTMLInputElement,
+		private stdOut: HTMLTextAreaElement,
+		private stdErr: HTMLTextAreaElement
+	) {
 		editor.onDidChangeModelContent((e) => {
 			clearTimeout(this.timeout);
 			this.timeout = window.setTimeout(this.doExec.bind(this), 700);
@@ -18,22 +24,27 @@ export class MainController {
 	private doExec() {
 		const result = this.execute(this.cmd.value, this.editor.getValue());
 
-		this.stdOut.textContent = (result.stdout || "").toString();
-		this.stdErr.textContent = (result.stderr || "").toString();
+		const stdout = (result.stdout || "").toString();
+		const stderr = (result.stderr || "").toString();
 
-		if(result.status > 0) {
+		this.stdOut.textContent = stdout;
+		this.stdErr.textContent = stderr;
+
+		this.container.classList.toggle("noerror", stderr == "");
+
+		if (result.status > 0) {
 			this.stdOut.style.background = '#fee';
 			this.stdErr.style.background = '#fee';
 
-			document.title = 'Exit Code: ' + result.status;
+			document.title = `Standard Otter: Exit Code: ${result.status}`;
 		} else {
 			this.stdOut.style.background = '';
 			this.stdErr.style.background = '';
 
-			document.title = 'Standard Otter';
+			document.title = 'Standard Otter: OK';
 		}
 
-		if( result.status !== null ) {
+		if (result.status !== null) {
 			this.cmd.style.background = '';
 		} else {
 			this.cmd.style.background = '#fee';
--- ts/editor.ts
+++ ts/editor.ts
@@ -3,6 +3,7 @@ import { MainController } from "./MainController";
 export function init(editor: monaco.editor.IStandaloneCodeEditor) {
 	new MainController(
 		editor,
+		document.querySelector('.container') as HTMLInputElement,
 		document.getElementById('cmd') as HTMLInputElement,
 		document.getElementById('output') as HTMLTextAreaElement,
 		document.getElementById('error') as HTMLTextAreaElement,
